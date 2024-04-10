7087a189609d9301d08f1646d976ae17b271dbb2
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-04-10 23:53:53 +0100

    App

--- basketball-scoreboard/src/App.js
+++ basketball-scoreboard/src/App.js
@@ -1,5 +1,12 @@
+import Scoreboard from "./components/Scoreboard";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="app">
+      <Scoreboard title="home" teamName="Chicago Bulls" />
+      <Scoreboard title="away" teamName="LA Lakers" />
+    </main>
+  );
 };
 
 export default App;
